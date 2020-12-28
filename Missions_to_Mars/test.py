4c9784a5cc3c3f83ffe21827b925e762a5bbe316
beckyydo <47190773+beckyydo@users.noreply.github.com>
2020-12-27 19:53:32 -0500

    update

--- Missions_to_Mars/test.py
+++ Missions_to_Mars/test.py
@@ -1,10 +1,1 @@
-from flask import Flask, render_template, redirect
-import pymongo
-from flask_pymongo import PyMongo
-
-# Use PyMongo to establish Mongo connection
-conn = 'mongodb://localhost:27017/'
-client = pymongo.MongoClient(conn)
-
-# Declare the database
-db = client["Mars_db"]
+import scrape_mars
