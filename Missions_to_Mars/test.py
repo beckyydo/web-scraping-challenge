577e917f1a9b3cecba501535e5c9598a072850ce
beckyydo <47190773+beckyydo@users.noreply.github.com>
2020-12-28 15:45:00 -0500

    delete test files

--- Missions_to_Mars/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from bs4 import BeautifulSoup as bs
-import requests
-from splinter import Browser
-
-executable_path = {'executable_path': 'chromedriver',}
-browser = Browser('chrome', **executable_path, headless=False)
